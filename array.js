50d61dba95a8eff2e49bda6aeab26100f1d0d44a
ARMAN UMAR <97071435+ammangdeveloper@users.noreply.github.com>
2022-07-07 16:21:07 +0700

    Update array.js

--- array.js
+++ array.js
@@ -15,3 +15,24 @@ for(const hewan of array){
 
 //akses index tertentu
 console.log(array[0])
+
+//menambah elemen array
+array.push('kerbau')
+console.log(array)
+
+//menampilkan elemen dari belakang
+// for(let i=array.length; i>=1; i--){
+//     console.log(`[${i}. ${array.pop(i)}]`)
+// }
+
+//menampilkan elemen pertama
+// console.log(array.shift())
+
+//menghapus elemen array
+array.splice(1, 1)
+console.log(array)
+/*
+Metode lain yang bisa kita gunakan untuk memanipulasi data pada array adalah shift() 
+dan unshift(). Metode shift() digunakan untuk mengeluarkan elemen pertama dari array, 
+sementara unshift() digunakan untuk menambahkan elemen di awal array.
+*/
