4734e9370ffd0ae9d67ec49eb83ab9c93e11c6bf
ARMAN UMAR <97071435+ammangdeveloper@users.noreply.github.com>
2022-07-08 20:08:56 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,9 @@
+//cara lain buat function
+const nilai = () =>{
+  let kelasD = ["arman", "aco", "taslim"] 
+  let kelasC = ["parif", "masri"] 
+  const gabung = [...kelasD, ...kelasC]
+  return gabung
+
+}
+console.log(nilai())
